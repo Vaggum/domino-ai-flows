2233f50f8beb80dad6c167b9eecb3f2cdd5ea8bc
vaggum bhaskar <vaggum.bhaskar@capgemini.com>
2025-02-03 01:10:45 -0800

    Pushed from Domino: https://siwrkshp.partner-engineering-team-sandbox.domino.tech/workspace/vaggum-bhaskar/Domino-training-flow?executionId=679c44cea7a62b0585943eab

--- scripts/load-data-A.py
+++ scripts/load-data-A.py
@@ -9,6 +9,7 @@ print(f"input location@@@@@@ {input_location}")
 try:
     with open(input_location, "r") as file:
         input_csv = file.read()
+    print("it is in Try block")
 except:
     print("it is in exception")
     with open("/mnt/code/data/datasetA.csv", "r") as file:
