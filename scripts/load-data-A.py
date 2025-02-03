5198a7bf0ddea52d0d509125d2dce9e332ca5a65
vaggum bhaskar <vaggum.bhaskar@capgemini.com>
2025-02-03 01:19:40 -0800

    Pushed from Domino: https://siwrkshp.partner-engineering-team-sandbox.domino.tech/workspace/vaggum-bhaskar/Domino-training-flow?executionId=679c44cea7a62b0585943eab

--- scripts/load-data-A.py
+++ scripts/load-data-A.py
@@ -6,14 +6,11 @@ import pandas as pd
 input_name = "data_path"
 input_location = f"/workflow/inputs/{input_name}"
 print(f"input location@@@@@@ {input_location}")
-try:
-    with open(input_location, "r") as file:
-        input_csv = file.read()
-    print("it is in Try block")
-except:
-    print("it is in exception")
-    with open("/mnt/code/data/datasetA.csv", "r") as file:
-        input_csv = file.read()
+
+with open(input_location, "r") as file:
+    input_csv = file.read()
+
+
 # Read input csv to dataframe
 print("this is input_csv",input_csv)
 df = pd.read_csv(input_csv) 
